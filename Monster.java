18daf11a5d1b2a60a35b34c6cd70dd912fa978b9
MrsHines <34042849+MrsHines@users.noreply.github.com>
2019-03-18 22:00:14 -0500

    Starter Files

---
+++ Monster.java
@@ -0,0 +1,16 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*; 
+
+public interface Monster
+{
+	public int getHowBig();
+	public String getName();
+	public boolean isBigger(Monster other);	
+	public boolean isSmaller(Monster other);
+	public boolean namesTheSame(Monster other);
+}
---
+++ Skeleton.java
@@ -0,0 +1,18 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*; 
+
+public class Skeleton implements Monster
+{
+	//add instance variables	
+
+	//add a constructor
+
+	//add code to implement the Monster interface
+
+	//add a toString
+}
